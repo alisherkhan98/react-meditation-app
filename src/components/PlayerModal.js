c8c6801c31f88d082479b1d65267c2d64ace21dc
Alisher laptop <asherkhan982@gmail.com>
2022-11-05 16:28:46 +0100

    test 2 my library

--- src/components/PlayerModal.js
+++ src/components/PlayerModal.js
@@ -17,7 +17,7 @@ import { closePlayer } from "../features/modals/modalsSlice";
 
 // My imports
 import Timer from "./Timer";
-import { Timeit } from "react-timeit";
+import { Timeit } from "my-library-timeit";
 import Waves from "./GradientWaves";
 
 
